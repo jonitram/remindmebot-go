aae2a946e30a85ad1c4dc7a1474a43f2b7dd2e4e
Jon Martin <jonnitram@JonNitram.local>
2019-07-30 15:17:04 -0500

    logic pseudocoded in

--- remindmebot.go
+++ remindmebot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -30,6 +31,9 @@ var commandPrefix string
 // add goroutine to master list
 // send remindme confirmation message
 // react to remindme confirmation message with options
+// link to message is:
+// https://discordapp.com/channels/<guildID>/<channelID>/<messageID>
+// guildID == "" for PMs -> "@me" instead of guildID
 
 // on reaction:
 // check content for remindme confirmation update + author = the bot
@@ -73,16 +77,65 @@ func errCheck(msg string, err error) {
 
 func messageHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
 	if strings.HasPrefix(message.Content, commandPrefix) {
-		fmt.Printf("ChannelID: %s Username: %s MessageID: %s Content: %s\n", message.ChannelID, message.Author.Username, message.ID, message.Content)
+		fmt.Printf("GuildID: %s ChannelID: %s Timestamp: %s Username: %s MessageID: %s Content: %s Current Time: %s\n", message.GuildID, message.ChannelID, message.Timestamp, message.Author.Username, message.ID, message.Content, time.Now().UTC().String())
+		// var time string
+		var reminder string
+		parameters := strings.TrimSpace(message.Content[len(commandPrefix):])
+		// check for containing "
+		// check indices, if the two are equal spit out error otherwise handle normally
+		if strings.IndexByte(parameters, '"') < strings.LastIndexByte(parameters, '"')+1 {
+			reminder = parameters[strings.IndexByte(parameters, '"') : strings.LastIndexByte(parameters, '"')+1]
+		}
+		fmt.Printf(reminder+"%d\n", len(reminder))
+		// keep "" in reminder and trim it out of parameters -> extract the time
+		// if regexp.MustCompile(commandPrefix + "\\s+.*\\s+\\\".*\\\"").MatchString(message.Content) {
+		// time = strings.TrimSpace(message.Content[len(commandPrefix):strings.IndexByte(message.Content, '"')])
+		// reminder = strings.TrimSpace(message.Content[strings.IndexByte(message.Content, '"')-1:])
+		// // len time == 0 if no
+		// fmt.Printf(time+" "+reminder+"%d", len(time))
+
+		// 		// go remindMe(message.Message)
+		// 	} else {
+		// 		// not correct command formatting
+		// 	}
 	}
+}
+
+func sortDuration(unit string) int {
+	unit = strings.ToLower(unit)
+	if strings.HasPrefix(unit, "s") {
+		return 1
+	} else if strings.HasPrefix(unit, "mi") {
+		return 60
+	} else if strings.HasPrefix(unit, "h") {
+		return 3600
+	} else if strings.HasPrefix(unit, "d") {
+		return 86400
+	} else if strings.HasPrefix(unit, "w") {
+		return 604800
+	} else if strings.HasPrefix(unit, "mo") {
+		return 2592000
+	} else if strings.HasPrefix(unit, "y") {
+		return 31104000
+	}
+	return -1
+}
+
+func remindMe(message *discordgo.Message) {
+	// parameters := regexp.MustCompile("\\s+").Split(message.Content, 4)
+	// time, err := strconv.Atoi(parameters[1])
+	// multiplier := sortDuration(parameters[2])
+
 }
 
 func main() {
 	setupTokens(tokensFile)
 	discord, err := discordgo.New("Bot " + discordToken)
 	errCheck("error creating discord session", err)
-	user, err := discord.User("@me")
-	commandPrefix = "<@" + user.ID + ">"
+	// user, err := discord.User(s"@me")
+	errCheck("error retrieving user from discord", err)
+	// commandPrefix = "<@" + user.ID + ">"
+	commandPrefix = "rm!"
 	fmt.Printf("Command Prefix: %s\n", commandPrefix)
 	discord.AddHandler(messageHandler)
 	errCheck("Error opening connection to Discord", discord.Open())
